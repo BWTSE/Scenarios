81168bd1186a40b77c1a774b021e56ff06a261f0
Hampus Broman <namorben@gmail.com>
2021-01-14 13:46:53 +0100

    changes indentations to spaces

--- shopping/low_debt/shopping/Item.java
+++ shopping/low_debt/shopping/Item.java
@@ -1,52 +1,52 @@
 package salestracker;
 
 public class Item {
-	
-	private int barcode;
-	private String name;
-	private double price;
-	private double quantity;
-	
-	public Item(int barcode, String name, double price) {
-		this.barcode = barcode;
-		this.name = name;
-		this.price = price;
-		this.quantity = 1;
-	}
 
-	
-	public int getBarcode() {
-		return barcode;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public double getPrice() {
-		return price;
-	}
-	
-	public double getQuantity() {
-		return quantity;
-	}
-	
-	public void incrementQuantity() {
-		quantity += 1;
-	}
-	
-	public void setQuantity(double newquantity) {
-		quantity = newquantity;
-	}
-	
-	
-	@Override
+    private int barcode;
+    private String name;
+    private double price;
+    private double quantity;
+
+    public Item(int barcode, String name, double price) {
+        this.barcode = barcode;
+        this.name = name;
+        this.price = price;
+        this.quantity = 1;
+    }
+
+
+    public int getBarcode() {
+        return barcode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void incrementQuantity() {
+        quantity += 1;
+    }
+
+    public void setQuantity(double newquantity) {
+        quantity = newquantity;
+    }
+
+
+    @Override
     public boolean equals(Object o) { 
-    	  
+
         if (o == this) { 
             return true; 
         } 
-  
+
         if (!(o instanceof Item)) { 
             return false; 
         } 
@@ -55,7 +55,7 @@ public class Item {
 
         return name == i.getName();
     }
-	
+
     @Override
     public int hashCode() {
         return this.barcode;
