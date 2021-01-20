062ab589023909e1e1a163a4204b37c5986f4896
williamleven <william@leven.id>
2021-01-20 17:25:13 +0100

    replace tabs with spaces

--- booking/low_debt/booking/Booking.java
+++ booking/low_debt/booking/Booking.java
@@ -5,73 +5,73 @@ import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
 public class Booking {
-	private final LocalDateTime start;
-	private final LocalDateTime end;
-	private final User customer;
-	private final Resource resource;
-	
-	public Booking(LocalDateTime start, LocalDateTime end, User customer, Resource resource) {
-		this.start = start;
-		this.end = end;
-		this.customer = customer;
-		this.resource = resource;
-	}
+    private final LocalDateTime start;
+    private final LocalDateTime end;
+    private final User customer;
+    private final Resource resource;
 
-	public LocalDateTime getStart() {
-	  return this.start;
-	}
+    public Booking(LocalDateTime start, LocalDateTime end, User customer, Resource resource) {
+        this.start = start;
+        this.end = end;
+        this.customer = customer;
+        this.resource = resource;
+    }
 
-	public LocalDateTime getEnd() {
-	  return this.end;
-	}
+    public LocalDateTime getStart() {
+        return this.start;
+    }
 
-	public User getCustomer() {
-	  return this.customer;
-	}
+    public LocalDateTime getEnd() {
+        return this.end;
+    }
 
-	public Resource getResource() {
-	  return this.resource;
-	}
+    public User getCustomer() {
+        return this.customer;
+    }
 
-	public boolean isDuring(LocalDateTime time) {
-		return !time.isBefore(this.getStart()) && !time.isAfter(this.getEnd());
-	}
+    public Resource getResource() {
+        return this.resource;
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) {
-			return true;
-		}
-		if (!(o instanceof Booking)) {
-			return false;
-		}
-		Booking booking = (Booking) o;
-		return Objects.equals(this.getStart(), booking.getStart()) &&
-				Objects.equals(this.getEnd(), booking.getEnd()) &&
-				Objects.equals(this.getCustomer(), booking.getCustomer()) &&
-				Objects.equals(this.getResource(), booking.getResource());
-	}
+    public boolean isDuring(LocalDateTime time) {
+        return !time.isBefore(this.getStart()) && !time.isAfter(this.getEnd());
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(
-				this.getStart(),
-				this.getEnd(),
-				this.getCustomer(),
-				this.getResource()
-		);
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Booking)) {
+            return false;
+        }
+        Booking booking = (Booking) o;
+        return Objects.equals(this.getStart(), booking.getStart()) &&
+                Objects.equals(this.getEnd(), booking.getEnd()) &&
+                Objects.equals(this.getCustomer(), booking.getCustomer()) &&
+                Objects.equals(this.getResource(), booking.getResource());
+    }
 
-	@Override
-	public String toString() {
-		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
-		return String.format(
-				"[%s to %s] (%s) booked by (%s)",
-				dateTimeFormatter.format(this.getStart()),
-				dateTimeFormatter.format(this.getEnd()),
-				this.getResource().toString(),
-				this.getCustomer().toString()
-		);
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(
+                this.getStart(),
+                this.getEnd(),
+                this.getCustomer(),
+                this.getResource()
+        );
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
+        return String.format(
+                "[%s to %s] (%s) booked by (%s)",
+                dateTimeFormatter.format(this.getStart()),
+                dateTimeFormatter.format(this.getEnd()),
+                this.getResource().toString(),
+                this.getCustomer().toString()
+        );
+    }
 
 }
--- booking/low_debt/booking/Resource.java
+++ booking/low_debt/booking/Resource.java
@@ -6,75 +6,75 @@ import java.util.List;
 import java.util.Objects;
 
 public abstract class Resource {
-	private final String name;
-	private final String description;
-	
-	private final List<Booking> bookings = new LinkedList<>();
-	
-	public Resource(String name, String description) {
-	    this.name = name;
-	    this.description = description;
-	}
+    private final String name;
+    private final String description;
 
-	public String getName() {
-		return this.name;
-	}
+    private final List<Booking> bookings = new LinkedList<>();
 
-	public String getDescription() {
-		return this.description;
-	}
-
-	public List<Booking> getBookings() {
-		return List.copyOf(this.bookings);
-	}
-
-	public boolean available(LocalDateTime start, LocalDateTime end) {
-		for (Booking booking: this.getBookings()) {
-			if (booking.isDuring(start)
-					|| booking.isDuring(end)
-					|| (booking.getStart().isAfter(start) && booking.getStart().isBefore(end))
-			) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	public Booking book (LocalDateTime start, LocalDateTime end, User customer) {
-        if (this.available(start, end)) {
-			Booking booking = new Booking(start, end, customer, this);
-        	bookings.add(booking);
-        	return booking;
-        } else {
-        	return null;
-		}
+    public Resource(String name, String description) {
+        this.name = name;
+        this.description = description;
     }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) {
-			return true;
-		}
-		if (!(o instanceof Resource)) {
-			return false;
-		}
-		Resource resource = (Resource) o;
-		return Objects.equals(this.getName(), resource.getName());
-	}
+    public String getName() {
+        return this.name;
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(this.getName());
-	}
+    public String getDescription() {
+        return this.description;
+    }
 
-	@Override
-	public String toString() {
-		return String.format(
-				"Resource %s \"%s\" #bookings: %s",
-				this.getName(),
-				this.getDescription(),
-				this.getBookings().size()
-		);
-	}
+    public List<Booking> getBookings() {
+        return List.copyOf(this.bookings);
+    }
+
+    public boolean available(LocalDateTime start, LocalDateTime end) {
+        for (Booking booking: this.getBookings()) {
+            if (booking.isDuring(start)
+                    || booking.isDuring(end)
+                    || (booking.getStart().isAfter(start) && booking.getStart().isBefore(end))
+            ) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public Booking book (LocalDateTime start, LocalDateTime end, User customer) {
+        if (this.available(start, end)) {
+            Booking booking = new Booking(start, end, customer, this);
+            bookings.add(booking);
+            return booking;
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Resource)) {
+            return false;
+        }
+        Resource resource = (Resource) o;
+        return Objects.equals(this.getName(), resource.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getName());
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Resource %s \"%s\" #bookings: %s",
+                this.getName(),
+                this.getDescription(),
+                this.getBookings().size()
+        );
+    }
 }
 
