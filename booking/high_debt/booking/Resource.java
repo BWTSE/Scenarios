21f70ad678bb02f753931270c27c625fc4c4a9dc
williamleven <william@leven.id>
2021-01-21 13:37:49 +0100

    start from low debt version

---
+++ booking/high_debt/booking/Resource.java
@@ -0,0 +1,77 @@
+package booking;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+public class Resource {
+    private final String name;
+    private final String description;
+
+    private final List<Booking> bookings = new LinkedList<>();
+
+    protected Resource(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public List<Booking> getBookings() {
+        return List.copyOf(this.bookings);
+    }
+
+    public boolean available(Interval interval) {
+        for (Booking booking : this.getBookings()) {
+            if (booking.getInterval().overlapsWith(interval)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public Optional<Booking> book(Interval interval, User customer) {
+        if (this.available(interval)) {
+            Booking booking = new Booking(interval, customer);
+            bookings.add(booking);
+            return Optional.of(booking);
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || this.getClass() != o.getClass()) {
+            return false;
+        }
+        Resource resource = (Resource) o;
+        return Objects.equals(this.getName(), resource.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getName());
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Resource %s \"%s\" #bookings: %s",
+                this.getName(),
+                this.getDescription(),
+                this.getBookings().size()
+        );
+    }
+}
+
---
+++ booking/high_debt/booking/User.java
@@ -0,0 +1,44 @@
+package booking;
+
+import java.util.Objects;
+
+public class User {
+
+    private final String name;
+    private final String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || this.getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return Objects.equals(getEmail(), user.getEmail());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getEmail());
+    }
+
+    @Override
+    public String toString() {
+        return String.format("User %s (%s)", this.getName(), this.getEmail());
+    }
+}
