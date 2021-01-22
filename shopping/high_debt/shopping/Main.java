0d3c79f7fa407808d621876b4dde18e1eee71fc4
Hampus Broman <namorben@gmail.com>
2021-01-22 14:15:17 +0100

    missed 1

--- shopping/high_debt/shopping/Main.java
+++ shopping/high_debt/shopping/Main.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package shopping;
 
 
@@ -17,27 +16,4 @@ public class Main {
         tracker.addItem(1007, 2.15);
         tracker.applyDiscounts();
     }
-=======
-package salestracker;
-
-
-public class Main {
-	
-	public static void main(String[] args) {
-		
-		Tracker tracker = new Tracker();
-		
-		int[] exampleBasket = new int[]{ 1001, 1001, 1002, 1004,1003,1005, 1003, 1002, 1001, 1005};
-		for (int barcode : exampleBasket) {
-			tracker.addItem(barcode);
-		}
-		tracker.addItem(1006, 0.5);
-		tracker.addItem(1007, 2.15);
-
-		
-		tracker.applyDiscounts();
-		tracker.printReceipt();
-		
-	}
->>>>>>> master
 }
